64d73819aa25eb4f8e13648da78dac752f52a12b
Jermaine <jermzblake@gmail.com>
2021-06-25 08:25:29 -0400

    add frontend boilerplate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import axios from 'axios'
+
 import './App.css';
 
+async function postImage({image, description}) {
+  const formData = new FormData();
+  formData.append("image", image)
+  formData.append("description", description)
+
+  const result = await axios.post('/images', formData, { headers: {'Content-Type': 'multipart/form-data'}})
+  return result.data
+}
+
+
 function App() {
+
+  const [file, setFile] = useState()
+  const [description, setDescription] = useState("")
+  const [images, setImages] = useState([])
+
+  const submit = async event => {
+    event.preventDefault()
+    console.log(description)
+    const result = await postImage({image: file, description})
+    setImages([result.image, ...images])
+  }
+
+  const fileSelected = event => {
+    const file = event.target.files[0]
+		setFile(file)
+	}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submit}>
+        <input onChange={fileSelected} type="file" accept="image/*"></input>
+        <input value={description} onChange={e => setDescription(e.target.value)} type="text"></input>
+        <button type="submit">Submit</button>
+      </form>
+
+      { images.map( image => (
+        <div key={image}>
+          <img src={image} alt="upload file"></img>
+        </div>
+      ))}
+
     </div>
   );
 }
