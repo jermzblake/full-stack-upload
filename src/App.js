46591f8414cf0d7d05353f5318e5193caa41a750
Jermaine <jermzblake@gmail.com>
2021-07-26 21:16:23 -0400

    add image preview

--- src/App.js
+++ src/App.js
@@ -1,50 +1,57 @@
-import { useState } from 'react';
-import axios from 'axios'
+import { useState } from "react";
+import axios from "axios";
 
-import './App.css';
+import "./App.css";
 
-async function postImage({image, description}) {
+async function postImage({ image, description }) {
   const formData = new FormData();
-  formData.append("image", image)
-  formData.append("description", description)
+  formData.append("image", image);
+  formData.append("description", description);
 
-  const result = await axios.post('/images', formData, { headers: {'Content-Type': 'multipart/form-data'}})
-  return result.data
+  const result = await axios.post("/images", formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+  return result.data;
 }
 
-
 function App() {
+  const [file, setFile] = useState();
+  const [description, setDescription] = useState("");
+  const [images, setImages] = useState([]);
+  const [previewImage, setPreviewImage] = useState("");
 
-  const [file, setFile] = useState()
-  const [description, setDescription] = useState("")
-  const [images, setImages] = useState([])
+  const submit = async (event) => {
+    event.preventDefault();
+    console.log(description);
+    const result = await postImage({ image: file, description });
+    setImages([result.image, ...images]);
+  };
 
-  const submit = async event => {
-    event.preventDefault()
-    console.log(description)
-    const result = await postImage({image: file, description})
-    setImages([result.image, ...images])
-  }
-
-  const fileSelected = event => {
-    const file = event.target.files[0]
-		setFile(file)
-	}
+  const fileSelected = (event) => {
+    const file = event.target.files[0];
+    setPreviewImage(URL.createObjectURL(event.target.files[0]));
+    setFile(file);
+  };
 
   return (
     <div className="App">
       <form onSubmit={submit}>
         <input onChange={fileSelected} type="file" accept="image/*"></input>
-        <input value={description} onChange={e => setDescription(e.target.value)} type="text"></input>
+        <input
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          type="text"
+        ></input>
         <button type="submit">Submit</button>
       </form>
 
-      { images.map( image => (
+      {images.map((image) => (
         <div key={image}>
           <img src={image} alt="upload file"></img>
         </div>
       ))}
 
+      <img src={previewImage} alt="preview" />
     </div>
   );
 }
